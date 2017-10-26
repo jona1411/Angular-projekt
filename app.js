daead06a7fe523dc6d402fc52ed05998b1f479f5
jona1411 <jonathan.klemensen@gmail.com>
2017-10-26 16:03:15 +0200

    added  more names and id

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 (function(){
-var app = angular.module('absenceList', []);
+    var app = angular.module('absenceList', []);
 
-app.controller('StudentsController', function($scope) {
-  $scope.studentList = students;
+    app.controller('StudentsController', function($scope) {
+        $scope.studentList = students;
 
-});
+    });
 
-var students =[
-{ name: 'Jens', absence: true},
-{ name: 'Henrik', absence: false},
-{ name: 'Peter',absence: false},
-{ name: 'Bastian', absence: true}
-];
+    var students =[
+        { name: 'Jacob', absence: true, id:1},
+        { name: 'Henrik', absence: false, id:2},
+        { name: 'Peter',absence: false, id:3},
+        { name: 'Bastian', absence: true, id:4},
+        { name: 'Frank', absence: true, id:5},
+        { name: 'Casper', absence: false, id: 6},
+        { name: 'Mia', absence: false, id: 7},
+        { name: 'Iben', absence: true, id: 8},
+        { name: 'Michael', absence: false, id: 9},
+        { name: 'Buster', absence: false, id: 10},
+        { name: 'Gob', abssence: false, id: 11},
+        { name: 'Lucille', absence: true, id: 12},
+        { name: 'George', absence: false, id: 13},
+        { name: 'Slick Rick', absence: true, id: 14}
+    ];
 
 
 
